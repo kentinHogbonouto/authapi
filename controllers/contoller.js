125a7aa6a6c838a23e55fc90d4307b4f4fd4b3a7
kentin HOGBONOUTO <kentinhogbonouto1@gmail.com>
2022-03-08 17:46:14 +0100

    update

--- controllers/contoller.js
+++ controllers/contoller.js
@@ -4,19 +4,21 @@
           {validationResult} = require('express-validator'),
           sendgridTransport = require('nodemailer-sendgrid-transport'),
           nodemailer = require('nodemailer'),
+          crypto = require('crypto'),
           sgMail = require('@sendgrid/mail');
     
     //post sign up middleware config
     exports.postSignUp =  (req , res) => {
-       const {firstName, lastName, email, password, confirmPassword } = req.body;
-       const salt = bcrypt.genSaltSync(10);
+      const {firstName, lastName, email, password } = req.body;
+      const confirmPassword = req.body.confirmPassword;
 
+       const salt = bcrypt.genSaltSync(10);
        User.findOne({email}).then(userDoc => {
            if(userDoc){
              return  res.json({"message" : "this email already exist"});
            }
            return bcrypt.hash(password , salt);
-           }).then(function(hashpassword){
+           }).then(hashpassword => {
                 const user = new User({
                 firstName,
                 lastName,
@@ -27,88 +29,64 @@
        }).catch(err =>{ 
            console.log(err);
         });
-       res.send({message : "Successfully sing up"});
+       res.json({message : "Successfully sing up"});
     };
 
     //post sign in middleware config
     exports.postSignIn = (req, res, next) => {
-      const email = req.body.email;
-      const password = req.body.password;
-    
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json({
-          "message" : "something went wrong"
-        });
-      }
-    
-      User.findOne({email})
-        .then(user => {
-          if (!user) {
-            return res.status(422).json({
-              "message" : "users don't exist" 
-            });
-          }
-          bcrypt
-            .compare(password, user.password)
-            .then(doMatch => {
-              if (doMatch) {
-                // req.session.isLoggedIn = true;
-                // req.session.user = user;
-                return req.session.save(err => {
-                  console.log(err);
-                  res.redirect('/');
-                });
-              }
-              return res.status(422).json({
-                firstName,
-                lastName,
-                email
-              });
-            })
-            .catch(err => {
-              console.log(err);
-            });
-        })
-        .catch(err => {
-          const error = new Error(err);
-          error.httpStatusCode = 500;
-          return next(error);
-        });
+     const {email , password}  = req.body;
+  
+      User.findOne({email}).then(userInfo =>{
+        if(!userInfo){
+          res.json({"message" : "user does'nt existe"});
+        }
+        bcrypt.compare(userInfo.password , password).then(data =>{
+          res.json({
+            firstName: userInfo.firstName,
+            lastName: userInfo.lastName,
+            email: userInfo.email
+          });
+        }).catch(err => console.log(err));
+      }).catch(err => console.log(err));
     };
 
-    //get sign in middleware config
-    exports.json({});
+    //reset mdp
     
-
-    //reset mdp    
+    const set_Mail_Apy_key = sgMail.setApiKey(process.env.SEND_GRID_MAIL);
+    const transporter = nodemailer.createTransport(
+      sendgridTransport({
+        auth: {
+          api_key: set_Mail_Apy_key
+        }
+      })
+    );
     //post reset
     exports.postReset = (req, res, next) => {
       crypto.randomBytes(32, (err, buffer) => {
+        const email = req.body.email;
         if (err) {
           console.log(err);
-          return res.redirect('/reset');
+          return res.json({"message" : "something went wrong ! check your script and try again"});
         }
         const token = buffer.toString('hex');
-        User.findOne({ email: req.body.email })
+        User.findOne({email})
           .then(user => {
             if (!user) {
-              req.flash('error', 'No account with that email found.');
-              return res.redirect('/reset');
+              req.json({'message' : 'No account with that email found.'});
             }
+            res.json({"message" : token});
             user.resetToken = token;
             user.resetTokenExpiration = Date.now() + 3600000;
             return user.save();
           })
           .then(result => {
-            res.redirect('/');
+            res.json({"message" : "reset email send successfully"});
             transporter.sendMail({
               to: req.body.email,
-              from: 'intside@corporation.com',
+              from: 'kentinhogbnouto1@gmail.com',
               subject: 'Password reset',
               html: `
-                <p>You requested a password reset</p>
-                <p>Click this <a href="http://localhost:8080/reset/${token}">link</a> to set a new password.</p>
+                <p>Click this <a href="http://localhost:8080/user-reset-pass/${token}">link</a> to set a new password.</p>
               `
             });
           })
@@ -119,29 +97,6 @@
           });
       });
     };
-    //get reset
-    exports.getReset = (req, res, next) => {
-      let message = req.flash('error');
-      if (message.length > 0) {
-        message = message[0];
-      } else {
-        message = null;
-      }
-      res.render('auth/reset', {
-        path: '/reset',
-        pageTitle: 'Reset Password',
-        errorMessage: message
-      });
-    };
-
-    const set_Mail_Apy_key = sgMail.setApiKey(process.env.SEND_GRID_MAIL);
-    const transporter = nodemailer.createTransport(
-      sendgridTransport({
-        auth: {
-          api_key: set_Mail_Apy_key
-        }
-      })
-    );
 
     //New password
     //get new password
--- routes/routes.js
+++ routes/routes.js
@@ -10,6 +10,7 @@
     //routes config
     router.post('/user-sign-up' , middlController.postSignUp);
     router.post('/user-sign-in' , middlController.postSignIn);
+    router.post('/user-reset-pass' , middlController.postReset);
 
     //export routes
     module.exports = router;
